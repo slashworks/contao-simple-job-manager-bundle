b07cc67b0ce1bfa9661ce4b9307df0b736ad020e
Simon Schwarz <simon@slash-works.de>
2020-01-20 09:56:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Contao 4 Simple Job Manager Bundle
+======================================
+
+Installation via local repository
+---------------------------------
+
+Run one of the following commands in your project directory:
+
+```bash
+php composer.phar require "slashworks/simple-job-manager-bundle @dev"
+```
+
+```bash
+composer require "slashworks/simple-job-manager-bundle @dev"
+```
---
+++ tests/ContaoSimpleJobManagerBundleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Slashworks\ContaoSimpleJobManagerBundle\Tests;
+
+use Slashworks\ContaoSimpleJobManagerBundle\ContaoSimpleJobManagerBundle;
+use PHPUnit\Framework\TestCase;
+
+class ContaoSimpleJobManagerBundleTest extends TestCase
+{
+    public function testCanBeInstantiated()
+    {
+        $bundle = new ContaoSimpleJobManagerBundleBundle();
+
+        $this->assertInstanceOf('Slashworks\ContaoSimpleJobManagerBundle\ContaoSimpleJobManagerBundle', $bundle);
+    }
+}
