d591cc901ea869d24d3466b4619610a20f7910f5
Simon Schwarz <simon@slash-works.de>
2020-01-20 10:17:43 +0100

    Initial commit

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,7 +14,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\SkeletonBundle\ContaoSkeletonBundle;
+use Slashworks\ContaoSimpleJobManagerBundle\ContaoSimpleJobManagerBundle;
 
 class Plugin implements BundlePluginInterface
 {
