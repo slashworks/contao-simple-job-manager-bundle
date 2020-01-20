8ea7eb21345c7820144768a957f767d46888e77d
Simon Schwarz <simon@slash-works.de>
2020-01-20 11:06:51 +0100

    Initital commit

--- src/ContaoSimpleJobManagerBundle.php
+++ src/ContaoSimpleJobManagerBundle.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\ContaoSimpleJobManagerBundle;
+namespace Slashworks\ContaoSimpleJobManagerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
