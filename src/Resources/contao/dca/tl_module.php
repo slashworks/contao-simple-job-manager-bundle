6f9348bcb8d480a3f3ee73436a280d7c886e1b3d
Stefan Melz <smelz@borowiakziehe.de>
2022-09-29 16:28:35 +0200

    fixed php8 problems

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -9,50 +9,49 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['job-reader'] = '{title_legend},name
 // Add fields to tl_module
 $GLOBALS['TL_DCA']['tl_module']['fields']['organisation'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['organisation'],
-    'exclude'                 => true,
-    'inputType'               => 'checkbox',
-    'eval'                    => array('multiple'=>true, 'mandatory'=>true, 'tl_class' => 'w50'),
-    'sql'                     => "blob NULL",
-    'foreignKey'              => 'tl_sjm_organisation.title',
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['organisation'],
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => array('multiple' => true, 'mandatory' => true, 'tl_class' => 'w50'),
+    'sql' => "blob NULL",
+    'foreignKey' => 'tl_sjm_organisation.title',
 );
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['expiredjob'] = array
 (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['expiredjob'],
-    'exclude'                 => true,
-    'inputType'               => 'checkbox',
-    'eval'                    => array('tl_class' => 'w50'),
-    'sql'                     => "int(1) NULL",
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['expiredjob'],
+    'exclude' => true,
+    'inputType' => 'checkbox',
+    'eval' => array('tl_class' => 'w50'),
+    'sql' => "int(1) NULL",
 );
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['jobsorting'] = array
 (
-    'label'     => &$GLOBALS['TL_LANG']['tl_module']['jobsorting'],
-    'exclude'   => true,
-    'search'    => true,
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['jobsorting'],
+    'exclude' => true,
+    'search' => true,
     'inputType' => 'select',
-    'options'   => $GLOBALS['sjm']['jobsorting']['options'],
+    'options' => $GLOBALS['sjm']['jobsorting']['options'],
     'reference' => &$GLOBALS['TL_LANG']['jobsorting'],
-    'eval'      => array('mandatory'=>true, 'maxlength'=>64, 'tl_class'=>'clr w50'),
-    'sql'       => "varchar(64) NOT NULL default ''",
+    'eval' => array('mandatory' => true, 'maxlength' => 64, 'tl_class' => 'clr w50'),
+    'sql' => "varchar(64) NOT NULL default ''",
 );
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['sortorder'] = array
 (
-    'label'     => &$GLOBALS['TL_LANG']['tl_module']['sortorder'],
-    'exclude'   => true,
-    'search'    => true,
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['sortorder'],
+    'exclude' => true,
+    'search' => true,
     'inputType' => 'select',
-    'options'   => array('ASC', 'DESC'),
+    'options' => array('ASC', 'DESC'),
     'reference' => &$GLOBALS['TL_LANG']['sortorder'],
-    'eval'      => array('mandatory'=>true, 'maxlength'=>64, 'tl_class'=>'w50'),
-    'sql'       => "varchar(64) NOT NULL default ''",
+    'eval' => array('mandatory' => true, 'maxlength' => 64, 'tl_class' => 'w50'),
+    'sql' => "varchar(64) NOT NULL default ''",
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] = $GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] . ' clr';
 
-Class tl_module_dca
+class tl_module_dca
 {
 
     /**
@@ -64,15 +63,14 @@ Class tl_module_dca
     {
         $objMod = \Contao\ModuleModel::findByPk($dc->id);
 
-        if ($objMod === null)
-        {
+        if ($objMod === null) {
             return;
         }
 
-        switch ($objMod->type)
-        {
+        switch ($objMod->type) {
             case 'job-list':
-                $GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] = $GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] . ' clr w50';
+                $GLOBALS['TL_DCA']['tl_module']['fields']['jumpTo']['eval']['tl_class'] = ' clr w50';
+
                 break;
 
         }
